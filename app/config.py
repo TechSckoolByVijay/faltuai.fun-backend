e7e22d4ba140bc88d7e4efc0440078e03b50acda
Vijay Saini <vijaysainiprofessional@gmail.com>
2025-12-12 01:39:28 +0530

    Fix CORS configuration to allow GitHub Pages origin

--- app/config.py
+++ app/config.py
@@ -31,7 +31,7 @@ class Settings:
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
         FRONTEND_URL,
-        # TODO: Add production URLs here
+        "https://techsckoolbyvijay.github.io",  # GitHub Pages root
     ]
     
     # Database Configuration (for future use)
